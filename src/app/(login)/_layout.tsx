7443be50579e7cf95eed1475523c02840ea9bdb4
Your Name <you@example.com>
2026-04-13 21:38:51 -0300

    aula ultima

--- src/app/(login)/_layout.tsx
+++ src/app/(login)/_layout.tsx
@@ -1,41 +1,78 @@
-import icons from "@/src/constants/icons";
+
 import { Stack } from "expo-router";
 import { Image } from "react-native";
 import "../../style/global.css";
+import icons from "@/src/constants/icons";
 
 export default function LoginLayout() {
-  return(  <Stack screenOptions={{
-    headerStyle:{ backgroundColor: "#ffffff" },
+  return (<Stack screenOptions={{
+    headerStyle: { backgroundColor: "#ffffff" },
     contentStyle: { backgroundColor: "#ffffff" },
   }} >
     <Stack.Screen
       name="login"
       options={{
         headerTitle: () => (
-          <Image source={icons.logo01}/>
+          <Image source={icons.logo01} />
         ),
         headerTitleAlign: 'center',
-        headerShadowVisible:false
+        headerShadowVisible: false
       }}
     />
-        <Stack.Screen
+    <Stack.Screen
       name="register"
       options={{
         headerTitle: () => (
-          <Image source={icons.logo01}/>
+          <Image source={icons.logo01} />
         ),
         headerTitleAlign: 'center',
-        headerShadowVisible:false
+        headerShadowVisible: false
       }}
     />
-        <Stack.Screen
+    <Stack.Screen
       name="address"
       options={{
         headerTitle: () => (
-          <Image source={icons.logo01}/>
+          <Image source={icons.logo01} />
         ),
         headerTitleAlign: 'center',
-        headerShadowVisible:false
+        headerShadowVisible: false
+      }}
+    />
+    <Stack.Screen
+      name="busca"
+      options={{
+        headerTitle: () => (
+          <Image source={icons.logo01}
+            style={{ width: 232, height: 42, marginTop: 10 }} />
+        ),
+        // headerShown: false,
+        headerTitleAlign: 'center',
+        headerShadowVisible: false
+      }}
+    />
+    <Stack.Screen
+      name="cardapio"
+      options={{
+        headerTitle: () => (
+          <Image source={icons.logo01}
+            style={{ width: 232, height: 42, marginTop: 10 }} />
+        ),
+        headerShown: false,
+        headerTitleAlign: 'center',
+        headerShadowVisible: false
+      }}
+    />
+    <Stack.Screen
+      name="detalhes"
+      options={{
+        headerTitle: () => (
+          <Image source={icons.logo01}
+            style={{ width: 232, height: 42, marginTop: 10 }} />
+        ),
+        headerShown: false,
+        headerTitleAlign: 'center',
+        headerShadowVisible: false
       }}
     />
   </Stack >
